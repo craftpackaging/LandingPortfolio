2028a366c82e2ffc0af2ac2eb5675037d2836be7
Rahul <rahulbhatia@Rahuls-MacBook-Pro.local>
2021-05-25 00:14:27 +0530

    Updates

---
+++ precache-manifest.543b2bd474f59eb70829ce7bd0915827.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2d8c4244ae996fcc4f7d5e3a77c8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2372bab9182d4dfd49b",
+    "url": "/static/css/2.70e35f80.chunk.css"
+  },
+  {
+    "revision": "909cd303a14efdbd0041",
+    "url": "/static/css/main.55d63c1b.chunk.css"
+  },
+  {
+    "revision": "f2372bab9182d4dfd49b",
+    "url": "/static/js/2.049c1fe1.chunk.js"
+  },
+  {
+    "revision": "c309a0a8222cb38a7f72b62ee15e4219",
+    "url": "/static/js/2.049c1fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909cd303a14efdbd0041",
+    "url": "/static/js/main.337b11e3.chunk.js"
+  },
+  {
+    "revision": "e22fd322f0b45aec7fb4",
+    "url": "/static/js/runtime-main.a7f6340d.js"
+  },
+  {
+    "revision": "83dff4f054d0b17e818fc345f7761b8c",
+    "url": "/static/media/Image1.83dff4f0.png"
+  },
+  {
+    "revision": "ba107077c95627d2cc01f9f77613fe68",
+    "url": "/static/media/Image10.ba107077.png"
+  },
+  {
+    "revision": "33466c0647391dac4964479eaef4e9f3",
+    "url": "/static/media/Image11.33466c06.png"
+  },
+  {
+    "revision": "70d4abf0a5b9b4684669728a3937f5f1",
+    "url": "/static/media/Image12.70d4abf0.png"
+  },
+  {
+    "revision": "f88974c1ea68c7af3d8720bd3469626b",
+    "url": "/static/media/Image2.f88974c1.png"
+  },
+  {
+    "revision": "95ed1dab589c87910ffbd4d9e9ee6bb3",
+    "url": "/static/media/Image3.95ed1dab.png"
+  },
+  {
+    "revision": "3d0e5987029b3d43401364123c0c12f2",
+    "url": "/static/media/Image4.3d0e5987.png"
+  },
+  {
+    "revision": "1ce24a53cd0cbd2ec86d878e938fda16",
+    "url": "/static/media/Image5.1ce24a53.png"
+  },
+  {
+    "revision": "2c3801a83f5867c5ee0e3099efb05dc9",
+    "url": "/static/media/Image6.2c3801a8.png"
+  },
+  {
+    "revision": "35ec11c67c4d760dde0da2855ef87dc1",
+    "url": "/static/media/Image7.35ec11c6.png"
+  },
+  {
+    "revision": "a8831c5bf59ab955c103bc6c66885a94",
+    "url": "/static/media/Image8.a8831c5b.png"
+  },
+  {
+    "revision": "993d073780ab062bf537f3852fbbce91",
+    "url": "/static/media/Image9.993d0737.png"
+  },
+  {
+    "revision": "da7e662fc206ef1bace5de8f930fbd42",
+    "url": "/static/media/PandG.da7e662f.png"
+  },
+  {
+    "revision": "8d6d774260175c0032c3cac40073a377",
+    "url": "/static/media/abbott.8d6d7742.png"
+  },
+  {
+    "revision": "89712720ffae86785b1eba6f030564ad",
+    "url": "/static/media/camlin.89712720.png"
+  },
+  {
+    "revision": "4ea4877b79afbecc58aba41e2ff84d6f",
+    "url": "/static/media/concept.4ea4877b.svg"
+  },
+  {
+    "revision": "789bbd58dd35ec4ea481dc77884d6bcf",
+    "url": "/static/media/distribution.789bbd58.svg"
+  },
+  {
+    "revision": "9c214310873d7f5d7c687385a636ac01",
+    "url": "/static/media/industry.9c214310.svg"
+  },
+  {
+    "revision": "e9a7e47cf90d9b5e6e542b92004bb1ed",
+    "url": "/static/media/logo.e9a7e47c.png"
+  },
+  {
+    "revision": "9136a032a19893959dba1e7b1ffe489a",
+    "url": "/static/media/maggi.9136a032.png"
+  },
+  {
+    "revision": "d4782ffdb8cbd3840d1ee8daa4f03842",
+    "url": "/static/media/manufacture.d4782ffd.svg"
+  },
+  {
+    "revision": "d83d2e3d6a02d58072f67a2f1fee5bb8",
+    "url": "/static/media/products1.d83d2e3d.png"
+  },
+  {
+    "revision": "f571a56d30b8d67e45c3a2425a971c81",
+    "url": "/static/media/products2.f571a56d.png"
+  },
+  {
+    "revision": "52800efac08923b20977d41fdc3e9393",
+    "url": "/static/media/products3.52800efa.png"
+  },
+  {
+    "revision": "09be041f1d95196326c5d1857154a1a4",
+    "url": "/static/media/supplies.09be041f.svg"
+  }
+]);
